c70ea8852d065b2765d806dcef5ed98992663005
brantrique <brantrique@outlook.com>
2016-12-08 22:28:24 -0200

    Modificacoes no Pretty Printer, testes ainda nao concluidos

---
+++ MiniHaskell/src-tests/br/unb/poo/mh/testes/TestePrettyPrinter.java
@@ -0,0 +1,163 @@
+package br.unb.poo.mh.testes;
+
+import br.unb.poo.mh.Expressao;
+import br.unb.poo.mh.PrettyPrinter;
+import br.unb.poo.mh.ValorBooleano;
+
+public class TestePrettyPrinter {
+	
+	@Test
+	public void testePrettyPrinterMatematicas(){
+		Expressao v10 = new ValorInteiro(10);
+		Expressao v5 = new ValorInteiro(5);
+		Expressao soma = new ExpressaoSoma(v10,v5);
+		Expressao sub = new ExpressaoSubtracao(v10,v5);
+		Expressao div = new ExpressaoDivisao(soma,sub);
+		Expressao mult = new ExpressaoMultiplicacao(div, v10);
+		
+		PrettyPrinter PP = new PrettyPrinter();
+		
+		v10.aceitar(PP);
+		Assert.assertEquals("10", PP.getPP());
+		PP.reset();
+		
+		v5.aceitar(PP);
+		Assert.assertEquals("5", PP.getPP());
+		PP.reset();
+		
+		soma.aceitar(PP);
+		Assert.assertEquals("(10+5)", PP.getPP());
+		PP.reset();
+		
+		sub.aceitar(PP);
+		Assert.assertEquals("(10-5)", PP.getPP());
+		PP.reset();
+		
+		div.aceitar(PP);
+		Assert.assertEquals("((10+5)/(10-5))", PP.getPP());
+		PP.reset();
+		
+		mult.aceitar(PP);
+		Assert.assertEquals("(((10+5)/(10-5))*10)", PP.getPP());
+	}
+	
+	@Test
+	public void testePrettyPrinterLogicas(){
+		Expressao f = new ValorBooleano(false);
+		Expressao v = new ValorBooleano(true);
+		Expressao not = new ExpressaoNot(f);
+		Expressao and = new ExpressaoAnd(not,v);
+		Expressao or = new ExpressaoOr(and,f);
+		
+		PrettyPrinter PP = new PrettyPrinter();
+		
+		f.aceitar(PP);
+		Assert.assertEquals("false", PP.getPP());
+		PP.reset();
+		
+		v.aceitar(PP);
+		Assert.assertEquals("true", PP.getPP());
+		PP.reset();
+		
+		not.aceitar(PP);
+		Assert.assertEquals("!(false)", PP.getPP());
+		PP.reset();
+		
+		and.aceitar(PP);
+		Assert.assertEquals("(!(false)&&true)", PP.getPP());
+		PP.reset();
+		
+		or.aceitar(PP);
+		Assert.assertEquals("((!(false)&&true)||false)", PP.getPP());
+	}
+	
+	@Test
+	public void testePrettyPrinterAritmeticas(){
+		ValorBooleano v = new ValorBooleano(true);
+		ValorBooleano f = new ValorBooleano(false);
+		Expressao v10 = new ValorInteiro(10);
+		Expressao v5 = new ValorInteiro(5);
+		Expressao v2 = new ValorInteiro(2);
+		Expressao maior = new ExpressaoMaior(v10,v5);
+		Expressao maiorigual = new ExpressaoMaiorIgual(v5,v5);
+		Expressao and1 = new ExpressaoAnd(maior,maiorigual);
+		Expressao menor = new ExpressaoMenor(v5,v2);
+		Expressao menorigual = new ExpressaoMenorIgual(v2,v5);
+		Expressao and2 = new ExpressaoAnd(menor, menorigual);
+		Expressao igual = new ExpressaoIgual(v10,v5);
+		Expressao diferente = new ExpressaoDiferente(v2,v2);
+		Expressao and3 = new ExpressaoAnd(igual,diferente);
+		Expressao or1 = new ExpressaoOr(and1,and2);
+		Expressao fim = new ExpressaoOr(or1, and3);
+		
+		PrettyPrinter PP = new PrettyPrinter();
+			
+		v2.aceitar(PP);
+		Assert.assertEquals("2", PP.getPP());
+		PP.reset();
+		
+		maior.aceitar(PP);
+		Assert.assertEquals("(10>5)", PP.getPP());
+		PP.reset();
+		
+		maiorigual.aceitar(PP);
+		Assert.assertEquals("(5>=5)", PP.getPP());
+		PP.reset();
+		
+		menor.aceitar(PP);
+		Assert.assertEquals("(5<2)", PP.getPP());
+		PP.reset();
+		
+		menorigual.aceitar(PP);
+		Assert.assertEquals("(2<=5)", PP.getPP());
+		PP.reset();
+		
+		igual.aceitar(PP);
+		Assert.assertEquals("(10==5)", PP.getPP());
+		PP.reset();
+		
+		diferente.aceitar(PP);
+		Assert.assertEquals("(2/=2)", PP.getPP());
+		PP.reset();
+		
+		fim.aceitar(PP);
+		Assert.assertEquals("((((10>5)&&(5>=5))||(((5<2)&&(2<=5)))||((10==5)&&(2/=2)))", PP.getPP());
+	}
+	
+	@Test
+	public void testeIfThenElse(){
+		Expressao v10 = new ValorInteiro(10);
+		Expressao v5 = new ValorInteiro(5);
+		Expressao maior = new ExpressaoMaior(v10,v5);
+		Expressao soma = new ExpressaoSoma(v10,v5);
+		Expressao sub = new ExpressaoSubtracao(v10,v5);
+		Expressao ifthenelse = new IfThenElse(maior,sub,soma);
+		
+		PrettyPrinter PP = new PrettyPrinter();
+		
+		ifthenelse.aceitar(PP);
+		Assert.assertEquals("if((10>5)) then (10-5) else (10+5)", PP.getPP());
+	}
+	
+	@Test
+	public void testeExpressaoLet(){
+		Expressao id = new Identificador("x");
+		Expressao v20 = new ValorInteiro(20);
+		Expressao v30 = new ValorInteiro(30);
+		Expressao soma = new ExpressaoSoma(id,v30);
+		Expressao let = new ExpressaoLet("x",v20,soma);
+		
+		id.aceitar(PP);
+		Assert.assertEquals("x", PP.getPP());
+		PP.reset();
+		
+		let.aceitar(PP);
+		Assert.assertEquals("let x = 20 in (x+30)", PP.getPP());	
+	}
+	
+	@Test
+	public void testeAplicacaoFuncao(){
+		
+	}
+
+}
